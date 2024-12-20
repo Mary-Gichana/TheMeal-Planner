c89b568138c628ccb3f9638eb5df4339874d6906
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-20 16:20:42 +0300

    created meal class and added __init__ to initialize an meal instance

--- models/meal.py
+++ models/meal.py
@@ -1,2 +1,9 @@
 import sqlite3
 from app import DATABASE_NAME
+
+class Meal:
+    def __init__(self, id=None, name=None, date=None, mealcategory_id=None):
+        self.id = id
+        self.name = name
+        self.date = date
+        self.mealcategory_id = mealcategory_id

d252c443b177436007d5e1d065565670caa8a322
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-20 16:47:13 +0300

    Added the validate_date function to ensure the input date follows the 'YYYY-MM-DD' format.

--- cli.py
+++ cli.py
@@ -5,3 +5,10 @@ from models.meal import Meal
 from models.mealcategory import MealCategory
 
 create_database()
+
+def validate_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%Y-%m-%d').date()
+    except ValueError:
+        print("Invalid date format. Use YYYY-MM-DD.")
+        return None

acb156900f19cb905727c21eef4b7f28bda829f2
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-20 16:06:40 +0300

    Added function to add a new meal category

--- models/mealcategory.py
+++ models/mealcategory.py
@@ -8,3 +8,9 @@ class MealCategory:
 
     def __repr__(self):
         return f"MealCategory({self.id}, {self.name})"
+    
+    def add_category(name):
+        with sqlite3.connect(DATABASE_NAME) as conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO mealcategory (name) VALUES (?)', (name,))
+            conn.commit()

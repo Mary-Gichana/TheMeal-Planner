f54cdfedcc5ebe2d6824b1537fdcbfe84e4ae4c9
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-20 16:04:55 +0300

    added repr method

--- models/mealcategory.py
+++ models/mealcategory.py
@@ -5,3 +5,6 @@ class MealCategory:
     def __init__(self, id=None, name=None):
         self.id = id
         self.name = name
+
+    def __repr__(self):
+        return f"MealCategory({self.id}, {self.name})"

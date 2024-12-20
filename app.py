d3744d5e87673b231f75f4f10eed5b3522e9356e
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-20 15:54:45 +0300

    Add meals table creation to create_database functio

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 import sqlite3
 
-DATABASE_NAME = 'meals.db'
+DATABASE_NAME = 'database.db'
+
+def create_database():
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS mealcategory (
+                id INTEGER PRIMARY KEY ,
+                name TEXT UNIQUE NOT NULL
+            )
+        ''')
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS meals (
+                id INTEGER PRIMARY KEY ,
+                name TEXT NOT NULL,
+                date TEXT NOT NULL,
+                mealcategory_id INTEGER NOT NULL,
+                FOREIGN KEY(mealcategory_id) REFERENCES mealcategory(id) 
+            )
+        ''')
